970e59d1a66783d10c10c5d88d845b3480cbc88e
zobkazi <zobaidulkazi64.official@gmail.com>
2024-07-01 17:23:10 +0600

    blog: create and get and limit logs init

--- src/app/api/blogs/create/route.ts
+++ src/app/api/blogs/create/route.ts
@@ -2,20 +2,54 @@ import { NextRequest, NextResponse } from "next/server";
 import { blogSchema } from "@/schemas/blogSchema";
 import Blog from "@/models/blog/Blog";
 import dbConnect from "@/utils/dbConnection";
-
+import { logger } from "@/lib/logger";
 
 
 export async function POST(request: NextRequest, response: NextResponse) {
+  try {
+    await dbConnect();
 
-  await dbConnect();
-  const body = await request.json();
+    const body = await request.json();
+    // validate request body
+    const parsedBody = blogSchema.safeParse(body);
+    if (!parsedBody.success) {
+     logger.warn("Blog validation failed",parsedBody.error);
+      return NextResponse.json({
+        success: false,
+        message: "Invalid request body",
+        error: parsedBody.error.errors,
+        
+      });
+      
+    }
 
+    // check if blog already exists
+    const blog = await Blog.findOne({ title: parsedBody.data.title });
+    if (blog) {
+      logger.info("Blog already exists");
+      return NextResponse.json({
+        success: false,
+        message: "Blog already exists",
+      });
+    }
 
-  
-  return NextResponse.json({
-    message: "Blog created successfully",
+    // create new blog
+    const newBlog = new Blog(parsedBody.data);
+    await newBlog.save();
+    logger.info("Blog created successfully");
+    return NextResponse.json({
+      success: true,
+      message: "Blog created successfully",
+      data: newBlog,
+      
+    });
 
-    blog: await Blog.create(body),
-
-  });
+  } catch (error) {
+    logger.error("Something went wrong", error);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong",
+      logger: error,
+    });
+  }
 }
--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,5 +1,43 @@
+// pages/api/blogs/index.ts
 import { NextRequest, NextResponse } from "next/server";
+import Blog from "@/models/blog/Blog";
+import dbConnect from "@/utils/dbConnection";
+import { logger } from "@/lib/logger";
 
 export async function GET(request: NextRequest) {
-  return NextResponse.json({ name: "John Doe" });
+  try {
+    await dbConnect();
+
+    const url = new URL(request.url);
+    const query = url.searchParams;
+
+    const page = parseInt(query.get("page") || "1", 10);
+    const limit = parseInt(query.get("limit") || "10", 10);
+    const skip = (page - 1) * limit;
+
+    const totalBlogs = await Blog.countDocuments();
+    const blogs = await Blog.find().skip(skip).limit(limit);
+
+    const totalPages = Math.ceil(totalBlogs / limit);
+
+    return NextResponse.json({
+      success: true,
+      data: {
+        blogs,
+        pagination: {
+          totalBlogs,
+          totalPages,
+          currentPage: page,
+          limit,
+        },
+      },
+    });
+  } catch (error) {
+    logger.error("Something went wrong", error);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
+  }
 }
