c9cedb9798fa719ef95fefb2684ad37c23e257c7
zobkazi <zobaidulkazi64.official@gmail.com>
2024-07-02 01:45:51 +0600

    image upload by local folder and not used packages..

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,13 @@
+import { UploadForm } from "@/components/ui-utils/UploadForm";
 import React from 'react'
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div>
+      <h1>Contact Page</h1>
+
+      <UploadForm />
+    </div>
   )
 }
 
