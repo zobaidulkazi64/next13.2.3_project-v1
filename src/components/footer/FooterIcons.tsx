602adc59def10b34d6bb09951a935cb92761ae23
zobkazi <zobaidulkazi64.official@gmail.com>
2024-07-31 06:02:34 +0600

    zsh: corrupt history file /home/zobaidul/.zsh_history

--- src/components/footer/FooterIcons.tsx
+++ src/components/footer/FooterIcons.tsx
@@ -5,7 +5,7 @@ import { socialLinks } from "@/contexts/socialLinks";
 const Footer: React.FC = () => {
   return (
     <footer className="py-8">
-      <div className="max-w-screen-xl mx-auto px-4 md:px-8 flex flex-col md:flex-row items-center justify-between">
+      <div className=" px-4 md:px-8 flex flex-col md:flex-row items-center justify-center">
         <div className="flex items-center space-x-4">
           {socialLinks.map(({ name, icon: Icon, link }) => (
             <Link
@@ -14,7 +14,7 @@ const Footer: React.FC = () => {
               aria-label={name}
               className="shadow-lg shadow-gray-500/50 p-2 rounded-full dark:text-gray-700 hover:text-gray-400"
             >
-              <Icon className="w-6 h-6" /> {/* Render the icon component */}
+              <Icon />
             </Link>
           ))}
         </div>
