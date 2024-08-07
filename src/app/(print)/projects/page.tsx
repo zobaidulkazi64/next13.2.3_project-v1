d2092cd89e45aa09a94fda6b717c454ef3123bff
zobkazi <zobaidulkazi64.official@gmail.com>
2024-08-08 00:10:35 +0600

    🦚:  OuterLayoutRouter webpack-internal

---
+++ src/app/(print)/projects/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProjectsPage = () => {
+  return (
+    <div>
+      <h1>Projects</h1>
+
+      <p className="text-gray-600 text-9xl justify-center">Coming Soon!</p>
+    </div>
+  );
+};
+
+export default ProjectsPage;
