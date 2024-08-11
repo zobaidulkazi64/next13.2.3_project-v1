093f690fbc6bc819d3e82de6cdf58fd733339792
Zobaidul Kazi <zobaidul@kazi.zobaidul>
2024-08-12 04:41:23 +0600

    🍥: src/app/(print)/blogs/[bId]/page.tsx

--- src/app/(print)/blogs/[bId]/page.tsx
+++ src/app/(print)/blogs/[bId]/page.tsx
@@ -1,9 +1,21 @@
 // src/app/(print)/blogs/[bId]/page.tsx
-
 import React from "react";
 
-const BlogPageById = () => {
-  return <div>BlogPageById</div>;
+// Simulate fetching blog IDs (replace this with actual data fetching)
+const fetchBlogIds = async () => {
+  return ["blog1", "blog2", "blog3"];
+};
+
+export async function generateStaticParams() {
+  const blogIds = await fetchBlogIds();
+
+  return blogIds.map((bId) => ({
+    bId,
+  }));
+}
+
+const BlogPageById = ({ params }: { params: { bId: string } }) => {
+  return <div>Blog ID: {params.bId}</div>;
 };
 
 export default BlogPageById;
